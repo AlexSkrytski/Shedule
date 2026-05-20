26cff3163a8bc6da98c6f008d8c7470da685d264
AlexSkrytski <alcoreservice@gmail.com>
2026-05-20 18:43:05 +0300

    Added new project

---
+++ HomeWork4Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeWork4Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
